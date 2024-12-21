dbd9f686079b5fec52403739146e389d01406ff5
Jamie Redding <jamie@coldhands.dev>
2024-12-21 10:36:55 +0000

    Day3: refactor to introduce Operation

--- src/main/kotlin/dev/coldhands/adventofcode/day3/Solution.kt
+++ src/main/kotlin/dev/coldhands/adventofcode/day3/Solution.kt
@@ -4,7 +4,7 @@ class Solution {
 
     private val regex: Regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
 
-    fun parseInput(input: String): List<Mul> {
+    fun parseInput(input: String): List<Operation> {
         return regex.findAll(input)
             .map { mul -> mul.groupValues.drop(1) }
             .map { args -> Mul(args[0].toInt(), args[1].toInt()) }
@@ -13,9 +13,15 @@ class Solution {
 
     fun computeInput(input: String): Int {
         return parseInput(input).asSequence()
+            .filter { it is Mul }
+            .map { it as Mul }
             .map { mul -> mul.first * mul.second }
             .sum()
     }
 
-    data class Mul(val first: Int, val second: Int)
+    sealed interface Operation
+
+    data class Mul(val first: Int, val second: Int) : Operation
+    data object Do : Operation
+    data object Dont: Operation
 }

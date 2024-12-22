94fac6674c0fc8d0feb0911c1edeaed5c828a9fc
Jamie Redding <jamie@coldhands.dev>
2024-12-22 17:32:19 +0000

    Day4: tidy up

--- src/main/kotlin/dev/coldhands/adventofcode/day4/Solution.kt
+++ src/main/kotlin/dev/coldhands/adventofcode/day4/Solution.kt
@@ -52,15 +52,6 @@ class Solution {
         return count
     }
 
-    /*
-
-    ..X...
-    .SAMX.
-    .A..A.
-    XMAS.S
-    .X....
-     */
-
     fun countOfXmasInWordSearch(input: String): Int {
         val lines = input.lines()
         val xMax: Int = lines.first().length - 1
@@ -79,93 +70,6 @@ class Solution {
             }
         }
         return total
-    }
-
-    private fun String.verticalLines(): List<String> {
-        val lines = mutableListOf<String>()
-
-        val horizontalLines = lines()
-        val xAxis = horizontalLines.first().length
-        val yAxis = horizontalLines.size
-        for (x in 0 until xAxis) {
-            val stringBuilder = StringBuilder()
-            for (y in 0 until yAxis) {
-                val char = horizontalLines[y][x]
-                stringBuilder.append(char)
-            }
-            lines.add(stringBuilder.toString())
-        }
-        return lines.toList()
-    }
-
-    private fun String.diagonalTopLeftLines(): List<String> {
-        val lines = mutableListOf<String>()
-
-        val horizontalLines = lines()
-        val xAxis = horizontalLines.first().length
-        val yAxis = horizontalLines.size
-
-        var x = 0
-        var y = 0
-        var farthestX = 0
-        var farthestY = 0
-        val stringBuilder = StringBuilder()
-        while (x < xAxis - 1 || y < yAxis) {
-            val char = horizontalLines[y][x]
-            stringBuilder.append(char)
-            if (y < yAxis - 1 && x != 0) {
-                y++
-                x--
-            } else {
-                y = if (farthestX == xAxis - 1) {
-                    farthestY++
-                    farthestY
-                } else {
-                    0
-                }
-                x = min(farthestX + 1, xAxis - 1)
-                farthestX = x
-                lines.add(stringBuilder.toString())
-                stringBuilder.clear()
-            }
-        }
-
-        return lines.toList()
-    }
-
-    private fun String.diagonalTopRightLines(): List<String> {
-        val lines = mutableListOf<String>()
-
-        val horizontalLines = lines()
-        val xAxis = horizontalLines.first().length
-        val yAxis = horizontalLines.size
-
-        var x = xAxis - 1
-        var y = 0
-        var farthestX = xAxis - 1
-        var farthestY = 0
-        val stringBuilder = StringBuilder()
-        while (x > 0 || y < yAxis) {
-            val char = horizontalLines[y][x]
-            stringBuilder.append(char)
-            if (y < yAxis - 1 && x != xAxis - 1) {
-                y++
-                x++
-            } else {
-                y = if (farthestX == 0) {
-                    farthestY++
-                    farthestY
-                } else {
-                    0
-                }
-                x = max(farthestX - 1, 0)
-                farthestX = x
-                lines.add(stringBuilder.toString())
-                stringBuilder.clear()
-            }
-        }
-
-        return lines.toList()
     }
 
 }

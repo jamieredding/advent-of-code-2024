7aa5f199ac8b0e7d93d5382714dc9695887c881f
Jamie Redding <jamie@coldhands.dev>
2024-12-23 22:13:09 +0000

    Day5: part 1

---
+++ src/main/kotlin/dev/coldhands/adventofcode/day5/Solution.kt
@@ -0,0 +1,33 @@
+package dev.coldhands.adventofcode.day5
+
+import dev.coldhands.adventofcode.day5.Input.*
+import java.util.TreeSet
+
+fun sort(update: Update, rules: List<PageOrderingRule>): Update {
+    val sorted: TreeSet<Int> = sortedSetOf(comparator(rules), *update.pages.toTypedArray())
+    return Update(sorted.toList())
+}
+
+fun comparator(rules: List<PageOrderingRule>): Comparator<Int> {
+    return object : Comparator<Int> {
+        override fun compare(o1: Int?, o2: Int?): Int {
+            return rules
+                .filter { it.before == o1 || it.after == o1 }
+                .filter { it.before == o2 || it.after == o2 }
+                .map { if (it.before == o1) -1 else 1 }
+                .first()
+        }
+    }
+}
+
+fun getOrderedUpdates(rules: List<PageOrderingRule>, updates: List<Update>): List<Update> {
+    return updates.filter { sort(it, rules) == it }
+}
+
+fun sumMiddlePageNumbers(updates: List<Update>): Int {
+    return updates.sumOf {
+        val size = it.pages.size
+        assert(size % 2 == 1)
+        it.pages[size / 2] // divide by 2 (don't need to add 1 as its zero indexed)
+    }
+}
---
+++ src/test/kotlin/dev/coldhands/adventofcode/day5/SolutionTest.kt
@@ -0,0 +1,87 @@
+package dev.coldhands.adventofcode.day5
+
+import dev.coldhands.adventofcode.day5.Input.PageOrderingRule
+import dev.coldhands.adventofcode.day5.Input.Update
+import dev.coldhands.adventofcode.readPersonalInput
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+
+    val rules = listOf(
+        PageOrderingRule(before = 47, after = 53),
+        PageOrderingRule(before = 97, after = 13),
+        PageOrderingRule(before = 97, after = 61),
+        PageOrderingRule(before = 97, after = 47),
+        PageOrderingRule(before = 75, after = 29),
+        PageOrderingRule(before = 61, after = 13),
+        PageOrderingRule(before = 75, after = 53),
+        PageOrderingRule(before = 29, after = 13),
+        PageOrderingRule(before = 97, after = 29),
+        PageOrderingRule(before = 53, after = 29),
+        PageOrderingRule(before = 61, after = 53),
+        PageOrderingRule(before = 97, after = 53),
+        PageOrderingRule(before = 61, after = 29),
+        PageOrderingRule(before = 47, after = 13),
+        PageOrderingRule(before = 75, after = 47),
+        PageOrderingRule(before = 97, after = 75),
+        PageOrderingRule(before = 47, after = 61),
+        PageOrderingRule(before = 75, after = 61),
+        PageOrderingRule(before = 47, after = 29),
+        PageOrderingRule(before = 75, after = 13),
+        PageOrderingRule(before = 53, after = 13)
+    )
+
+    val updates = listOf(
+        Update(pages = listOf(75, 47, 61, 53, 29)),
+        Update(pages = listOf(97, 61, 53, 29, 13)),
+        Update(pages = listOf(75, 29, 13)),
+        Update(pages = listOf(75, 97, 47, 61, 53)),
+        Update(pages = listOf(61, 13, 29)),
+        Update(pages = listOf(97, 13, 75, 29, 47)),
+    )
+
+    @Test
+    fun `sort update using rules 1`() {
+        val unsorted = Update(listOf(29, 47, 53, 61, 75))
+
+        sort(unsorted, rules) shouldBe Update(listOf(75, 47, 61, 53, 29))
+    }
+
+    @Test
+    fun `sort update using rules 2`() {
+        val unsorted = Update(listOf(13, 29, 53, 61, 97))
+
+        sort(unsorted, rules) shouldBe Update(listOf(97, 61, 53, 29, 13))
+    }
+
+    @Test
+    fun `filters only ordered updates`() {
+        getOrderedUpdates(rules, updates) shouldBe listOf(
+            Update(pages = listOf(75, 47, 61, 53, 29)),
+            Update(pages = listOf(97, 61, 53, 29, 13)),
+            Update(pages = listOf(75, 29, 13)),
+        )
+    }
+
+    @Test
+    fun `sum middle page numbers`() {
+        sumMiddlePageNumbers(listOf(
+            Update(pages = listOf(75, 47, 61, 53, 29)),
+            Update(pages = listOf(97, 61, 53, 29, 13)),
+            Update(pages = listOf(75, 29, 13)),
+        )) shouldBe 143
+    }
+
+    @Test
+    fun `part 1` () {
+        val inputString = readPersonalInput(this)
+
+        val input = Parser().parse(inputString)
+
+        val result = getOrderedUpdates(input.pageOrderingRules, input.updates)
+            .run { sumMiddlePageNumbers(this) }
+
+        result shouldBe 6267
+    }
+}
